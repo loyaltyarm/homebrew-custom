00fca95ffd1051eb17f35ef996de46d04ea8317f
loyaltyarm <loyaltyarm@gmail.com>
2021-03-17 00:56:50 -0700

    attempted these bazel versions in downgrade

---
+++ Formula/bazel@3.3.0.rb
@@ -0,0 +1,80 @@
+class BazelAT330 < Formula
+  desc "Google's own build tool"
+  homepage "https://bazel.build/"
+  url "https://github.com/bazelbuild/bazel/releases/download/3.3.0/bazel-3.3.0-dist.zip"
+  sha256 "05a03960de09d5775839c5766ad8a0a30f261feaba5fa53ce3e49168d1eee826"
+
+  depends_on "python@3.8" => :build
+  depends_on "openjdk@11"
+
+  uses_from_macos "zip"
+
+  def install
+    ENV["EMBED_LABEL"] = "#{version}-homebrew"
+    # Force Bazel ./compile.sh to put its temporary files in the buildpath
+    ENV["BAZEL_WRKDIR"] = buildpath/"work"
+    # Force Bazel to use openjdk@11
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_libexec/"openjdk.jdk/Contents/Home"
+    ENV["EXTRA_BAZEL_ARGS"] = "--host_javabase=@local_jdk//:jdk"
+
+    (buildpath/"sources").install buildpath.children
+
+    cd "sources" do
+      system "./compile.sh"
+      system "./output/bazel",
+             "--output_user_root",
+             buildpath/"output_user_root",
+             "build",
+             "scripts:bash_completion"
+
+      bin.install "scripts/packages/bazel.sh" => "bazel"
+      ln_s libexec/"bin/bazel-real", bin/"bazel-#{version}"
+      (libexec/"bin").install "output/bazel" => "bazel-real"
+      bin.env_script_all_files(libexec/"bin",
+        :JAVA_HOME => Formula["openjdk@11"].opt_libexec/"openjdk.jdk/Contents/Home")
+
+      bash_completion.install "bazel-bin/scripts/bazel-complete.bash"
+      zsh_completion.install "scripts/zsh_completion/_bazel"
+
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    touch testpath/"WORKSPACE"
+
+    (testpath/"ProjectRunner.java").write <<~EOS
+      public class ProjectRunner {
+        public static void main(String args[]) {
+          System.out.println("Hi!");
+        }
+      }
+    EOS
+
+    (testpath/"BUILD").write <<~EOS
+      java_binary(
+        name = "bazel-test",
+        srcs = glob(["*.java"]),
+        main_class = "ProjectRunner",
+      )
+    EOS
+
+    system bin/"bazel",
+           "build",
+           "//:bazel-test"
+    assert_equal "Hi!\n", pipe_output("bazel-bin/bazel-test")
+
+    # Verify that `bazel` invokes Bazel's wrapper script, which delegates to
+    # project-specific `tools/bazel` if present. Invoking `bazel-VERSION`
+    # bypasses this behavior.
+    (testpath/"tools"/"bazel").write <<~EOS
+      #!/bin/bash
+      echo "stub-wrapper"
+      exit 1
+    EOS
+    (testpath/"tools/bazel").chmod 0755
+
+    assert_equal "stub-wrapper\n", shell_output("#{bin}/bazel --version", 1)
+    assert_equal "bazel #{version}-homebrew\n", shell_output("#{bin}/bazel-#{version} --version")
+  end
+end
---
+++ Formula/bazel@3.3.1.rb
@@ -0,0 +1,81 @@
+class BazelAT331 < Formula
+  desc "Google's own build tool"
+  homepage "https://bazel.build/"
+  url "https://github.com/bazelbuild/bazel/releases/download/3.3.1/bazel-3.3.1-dist.zip"
+  sha256 "e0f1f43c65c4e0a38522b37e81f6129d8a1f7cd3d8884847be306544a7492747"
+  license "Apache-2.0"
+
+  depends_on "python@3.8" => :build
+  depends_on "openjdk@11"
+
+  uses_from_macos "zip"
+
+  def install
+    ENV["EMBED_LABEL"] = "#{version}-homebrew"
+    # Force Bazel ./compile.sh to put its temporary files in the buildpath
+    ENV["BAZEL_WRKDIR"] = buildpath/"work"
+    # Force Bazel to use openjdk@11
+    ENV["JAVA_HOME"] = Formula["openjdk@11"].opt_libexec/"openjdk.jdk/Contents/Home"
+    ENV["EXTRA_BAZEL_ARGS"] = "--host_javabase=@local_jdk//:jdk"
+
+    (buildpath/"sources").install buildpath.children
+
+    cd "sources" do
+      system "./compile.sh"
+      system "./output/bazel",
+             "--output_user_root",
+             buildpath/"output_user_root",
+             "build",
+             "scripts:bash_completion"
+
+      bin.install "scripts/packages/bazel.sh" => "bazel"
+      ln_s libexec/"bin/bazel-real", bin/"bazel-#{version}"
+      (libexec/"bin").install "output/bazel" => "bazel-real"
+      bin.env_script_all_files(libexec/"bin",
+        :JAVA_HOME => Formula["openjdk@11"].opt_libexec/"openjdk.jdk/Contents/Home")
+
+      bash_completion.install "bazel-bin/scripts/bazel-complete.bash"
+      zsh_completion.install "scripts/zsh_completion/_bazel"
+
+      prefix.install_metafiles
+    end
+  end
+
+  test do
+    touch testpath/"WORKSPACE"
+
+    (testpath/"ProjectRunner.java").write <<~EOS
+      public class ProjectRunner {
+        public static void main(String args[]) {
+          System.out.println("Hi!");
+        }
+      }
+    EOS
+
+    (testpath/"BUILD").write <<~EOS
+      java_binary(
+        name = "bazel-test",
+        srcs = glob(["*.java"]),
+        main_class = "ProjectRunner",
+      )
+    EOS
+
+    system bin/"bazel",
+           "build",
+           "//:bazel-test"
+    assert_equal "Hi!\n", pipe_output("bazel-bin/bazel-test")
+
+    # Verify that `bazel` invokes Bazel's wrapper script, which delegates to
+    # project-specific `tools/bazel` if present. Invoking `bazel-VERSION`
+    # bypasses this behavior.
+    (testpath/"tools"/"bazel").write <<~EOS
+      #!/bin/bash
+      echo "stub-wrapper"
+      exit 1
+    EOS
+    (testpath/"tools/bazel").chmod 0755
+
+    assert_equal "stub-wrapper\n", shell_output("#{bin}/bazel --version", 1)
+    assert_equal "bazel #{version}-homebrew\n", shell_output("#{bin}/bazel-#{version} --version")
+  end
+end
